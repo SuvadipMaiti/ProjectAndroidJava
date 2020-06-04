06e68eda2c174f68354d6fcb342f9b281978bcee
Suvadip Maiti <suvadip@gmail.com>
2020-06-04 12:20:52 +0530

    onBoard screen

--- app/src/main/java/com/example/projectandroidjava/Adapters/ViewPageAdapter.java
+++ app/src/main/java/com/example/projectandroidjava/Adapters/ViewPageAdapter.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
@@ -34,9 +36,9 @@ public class ViewPageAdapter extends PagerAdapter {
             "Title 3",
     };
     private String descs[] = {
-            "Desc 1",
-            "Desc 2",
-            "Desc 3",
+            "Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1 Desc 1",
+            "Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2 Desc 2",
+            "Desc 3 Desc 3 Desc 3 Desc 3 Desc 3 Desc 3 Desc 3",
     };
 
     @Override
@@ -52,6 +54,27 @@ public class ViewPageAdapter extends PagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        return super.instantiateItem(container, position);
+       inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+       View view = inflater.inflate(R.layout.view_pager,container,false);
+
+       //init view
+        ImageView imageView = view.findViewById(R.id.imageViewPager);
+        TextView textTitle = view.findViewById(R.id.txtTitleViewPaper);
+        TextView textDesc = view.findViewById(R.id.txtDescViewPaper);
+
+
+        imageView.setImageResource(images[position]);
+        textTitle.setText(titles[position]);
+        textDesc.setText(descs[position]);
+
+        container.addView(view);
+        return view;
+
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
     }
 }
--- app/src/main/java/com/example/projectandroidjava/OnboardActivity.java
+++ app/src/main/java/com/example/projectandroidjava/OnboardActivity.java
@@ -1,14 +1,115 @@
 package com.example.projectandroidjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.projectandroidjava.Adapters.ViewPageAdapter;
 
 public class OnboardActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private Button btnLeft,btnRight;
+    private ViewPageAdapter adapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboard);
+        init();
     }
+
+    private void init() {
+        viewPager = findViewById(R.id.view_pager);
+        btnLeft = findViewById(R.id.btnLeft);
+        btnRight = findViewById(R.id.btnRight);
+        dotsLayout = findViewById(R.id.dotsLayout);
+
+        adapter = new ViewPageAdapter(this);
+        addDots(0);
+        viewPager.addOnPageChangeListener(listener); //create this listener
+        viewPager.setAdapter(adapter);
+
+        btnRight.setOnClickListener(v->{
+            //if button text is next we will go next page of viewPager
+            if(btnRight.getText().toString().equals("Next")){
+                viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+            }else{
+                //else its finish , we will start Auth activity
+                startActivity(new Intent(OnboardActivity.this,AuthActivity.class));
+                finish();
+            }
+        });
+
+        btnLeft.setOnClickListener(v->{
+            //if button skip clicked then we go to page 3
+                viewPager.setCurrentItem(viewPager.getCurrentItem()+2);
+        });
+    }
+
+    //method to cat  dots from html code
+    private void addDots(int position){
+        dotsLayout.removeAllViews();
+        dots = new TextView[3];
+        for (int i =0; i < dots.length; i++){
+            dots[i] = new TextView(this);
+            //this html code cree dot
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(ContextCompat.getColor(this,R.color.colorLightGrey));
+
+            dotsLayout.addView(dots[i]);
+
+        }
+
+        //let change the select dot color
+        if(dots.length > 0){
+            dots[position].setTextColor(ContextCompat.getColor(this,R.color.colorGrey));
+        }
+    }
+
+    private ViewPager.OnPageChangeListener listener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            //change text of the Next button to Finish if page reach last position
+            //and hide the skip button if we not in page 1
+            if(position == 0){
+                btnLeft.setVisibility(ViewPager.VISIBLE);
+                btnLeft.setEnabled(true);
+                btnRight.setText("Next");
+            }else if(position == 1){
+                btnLeft.setVisibility(ViewPager.GONE);
+                btnLeft.setEnabled(false);
+                btnRight.setText("Next");
+
+            }else{
+                btnLeft.setVisibility(ViewPager.GONE);
+                btnLeft.setEnabled(false);
+                btnRight.setText("Finish");
+
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
 }
